a057cc0b6fb02ac84642245b945040dccd07825f
catmlima <catisousalima1999@gmail.com>
2024-02-18 20:31:33 +0000

    Now you can enter the EXACT location of the airport you are leaving from and the EXACT date (with hours and everything) from the database and it will print out your available seats  in that flight.

--- src/main/java/backend/DataExchange.java
+++ src/main/java/backend/DataExchange.java
@@ -4,7 +4,7 @@ import java.sql.*;
 
 public class DataExchange {
 
-    // This method focuses on getting a input location and date and showing the user which seats are available.
+    // This method focuses on getting an input location and date and showing the user which seats are available.
     // So far there is only one flight so don't expect much.
     public static void dbSearch(String location, String date) throws SQLException, ClassNotFoundException {
         Connection c = null;
@@ -12,7 +12,16 @@ public class DataExchange {
         try{
             c = DriverManager.getConnection("jdbc:sqlite:src/main/flightsystem.db");
 
+            // Testing that given the Location and Date of travels, the available seats will be printed out.
+            String sql = "Select Seat from Flights, Airport where Flights.FlightID = Airport.FlightID AND Location = (?) AND DepDate = (?) AND Taken=FALSE";
+            PreparedStatement preparedStatement = c.prepareStatement(sql);
+            preparedStatement.setString(1, location);
+            preparedStatement.setString(2,date);
+            ResultSet result = preparedStatement.executeQuery();
 
+            while(result.next()){
+                System.out.println(result.getString(1) + " is available");
+            }
 
 
             // Testing that the connection works by printing it.
