44ccf8c1a249980add8c75699c808f3e0a072577
catmlima <catisousalima1999@gmail.com>
2024-02-21 19:29:58 +0000

    Now you can enter the EXACT location of the airport you are leaving from and the EXACT date (with hours and everything) from the database and it will print out your available seats  in that flight.

---
+++ src/main/java/module-info-CatAndHremmaPC.java
@@ -0,0 +1,10 @@
+module catlima.todatabasefrominterfacetest {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+
+
+
+    opens catlima.todatabasefrominterfacetest to javafx.fxml;
+    exports catlima.todatabasefrominterfacetest;
+}
