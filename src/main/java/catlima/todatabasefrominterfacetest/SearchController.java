8ea64d0c0c5c8d149cdd9f1e8177d645aec97e9f
catmlima <catisousalima1999@gmail.com>
2024-02-21 19:52:57 +0000

    Have added DatePicker to the search-view.fxml and also date formatter to get the input to the database in the correct sql format.

--- src/main/java/catlima/todatabasefrominterfacetest/SearchController.java
+++ src/main/java/catlima/todatabasefrominterfacetest/SearchController.java
@@ -1,12 +1,17 @@
 package catlima.todatabasefrominterfacetest;
 
 import backend.DataExchange;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.chart.PieChart;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class SearchController {
 
@@ -17,11 +22,22 @@ public class SearchController {
     private TextField fxDate;
 
     @FXML
+    private DatePicker fxDepartureDate;
+
+    private String selectedDate;
+
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+
+    public void initialize(){
+        fxDepartureDate.setValue(LocalDate.now());
+    }
+
+    @FXML
     protected void onSearchClick(ActionEvent actionEvent) throws SQLException, ClassNotFoundException {
         String location = fxLocation.getText();
-        String date = fxDate.getText();
+        String date = fxDepartureDate.getValue().format(formatter);
 
-        if (location != null && date != null){
+        if (location != null){
             DataExchange.dbSearch(location,date);
         }
     }
