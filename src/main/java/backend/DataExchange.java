088649ea6a31e1f205ee45e891b21698e9b4663b
catmlima <catisousalima1999@gmail.com>
2024-02-25 16:09:37 +0000

    Now able to enter both where you are leaving from and where you are going and it accurately prints out.

--- src/main/java/backend/DataExchange.java
+++ src/main/java/backend/DataExchange.java
@@ -6,17 +6,18 @@ public class DataExchange {
 
     // This method focuses on getting an input location and date and showing the user which seats are available.
     // So far there is only one flight so don't expect much.
-    public static void dbSearch(String location, String date) throws SQLException, ClassNotFoundException {
+    public static void dbSearch(String location, String destination, String date) throws SQLException, ClassNotFoundException {
         Connection c = null;
         Class.forName("org.sqlite.JDBC");
         try{
             c = DriverManager.getConnection("jdbc:sqlite:src/main/flightsystem.db");
 
             // Testing that given the Location and Date of travels, the available seats will be printed out.
-            String sql = "Select Seat from Flights, Airport where Flights.FlightID = Airport.FlightID AND Location = (?) AND DepDate = (?) AND Taken=FALSE";
+            String sql = "Select Seat from Flights, Airport a1, Airport a2 where Flights.FlightID = a1.FlightID AND Flights.FlightID = a2.FlightID AND a1.Location = (?) AND a2.location = (?) AND DepDate = (?) AND Taken=FALSE";
             PreparedStatement preparedStatement = c.prepareStatement(sql);
             preparedStatement.setString(1, location);
-            preparedStatement.setString(2,date);
+            preparedStatement.setString(2,destination);
+            preparedStatement.setString(3,date);
             ResultSet result = preparedStatement.executeQuery();
 
 
--- src/main/java/catlima/todatabasefrominterfacetest/SearchController.java
+++ src/main/java/catlima/todatabasefrominterfacetest/SearchController.java
@@ -6,9 +6,11 @@ import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.chart.PieChart;
+import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import org.w3c.dom.Text;
 
 import java.sql.SQLException;
 import java.time.LocalDate;
@@ -23,23 +25,29 @@ public class SearchController {
     private TextField fxDate;
 
     @FXML
+    private TextField fxDestination;
+
+    @FXML
     private DatePicker fxDepartureDate;
 
-    private String selectedDate;
+    /* Add a binding for the Button fxSearch so they cannot search without selecting their location
+    * and destination. */
+
+    @FXML
+    private Button fxSearch;
 
 
-    public void initialize(){
-        fxDepartureDate.setValue(LocalDate.now());
-    }
+    public void initialize(){ fxDepartureDate.setValue(LocalDate.now());}
 
     @FXML
     protected void onSearchClick(ActionEvent actionEvent) throws SQLException, ClassNotFoundException {
         String location = fxLocation.getText();
         String date = String.valueOf(fxDepartureDate.getValue());
+        String destination =fxDestination.getText();
 
         // put "date" as second parameter.
         if (location != null){
-            DataExchange.dbSearch(location,date);
+            DataExchange.dbSearch(location,destination,date);
         }
 
     }
