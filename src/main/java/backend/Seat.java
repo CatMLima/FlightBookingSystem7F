a563a8220b577ac65075afb79c6971f2a0758033
brynjar <brynjar23@live.com>
2024-03-07 14:47:55 +0000

    Constructed and made getters'n'setters for Seat.java

--- src/main/java/backend/Seat.java
+++ src/main/java/backend/Seat.java
@@ -1,7 +1,46 @@
 package backend;
 
 public class Seat {
-    {
+    private String seatName;
+    private String Tier;
+    private Boolean isBooked;
+    private Boolean isonHold;
 
+    public Seat(String seatName, String Tier, Boolean isBooked, Boolean isonHold)   {
+        this.seatName = seatName;
+        this.Tier = Tier;
+        this.isBooked = isBooked;
+        this.isonHold = isonHold;
+    }
+
+    public Boolean getIsonHold() {
+        return isonHold;
+    }
+
+    public void setIsonHold(Boolean onHold) {
+        this.isonHold = onHold;
+    }
+
+    public Boolean getBooked() {
+        return isBooked;
+    }
+
+    public void setBooked(Boolean booked) {
+        isBooked = booked;
+    }
+
+    public String getTier() {
+        return Tier;
+    }
+
+    public void setTier(String tier) {
+        Tier = tier;
+    }
+
+    public String getSeatName() {
+        return seatName;
+    }
+    public void setSeatName(String seatname) {
+        seatName = seatName;
     }
 }
